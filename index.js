7c22536caad09f8beb62fac5ab82fd852b4bcc27
stundennull <stundennull@gmail.com>
2021-02-09 17:55:02 -0600

    commit tree 1ee627096d929349df3e4e5af8163cb541ec1006

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName
+
+function upperCaseCustomerName() {
+    customerName = s.toUpperCase();
+}
+
+function setBestCustomer() {
+    var bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = "bob";
+}
+
+const leastFavoriteCustomer = "bob"
+
+function changeLeastFavoriteCustomer() {
+    let leastFavoriteCustomer = "susan";
+}
